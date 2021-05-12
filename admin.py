b3b188e16796cb31585cfe0d01d11606ee52b166
Elvis-codeur <83865024+Elvis-codeur@users.noreply.github.com>
2021-05-12 14:32:29 +0000

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from second.models import *
+
+admin.site.register(LocationArticleSave)
+admin.site.register(VendeurEnregistrer)
+admin.site.register(ArticleEnregistrer)
+admin.site.register(ArticleCategorieSave)
+admin.site.register(AcheteurEnregistrer)
+# Register your models here.
